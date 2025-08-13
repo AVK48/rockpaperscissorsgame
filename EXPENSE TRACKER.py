c5a72af3a9242d663c8c26d7c96242ff4686007d
AENNAM VINAY KUMAR <aennamvinaykumar@gmail.com>
2025-08-13 21:13:46 +0530

    Update EXPENSE TRACKER.py

--- EXPENSE TRACKER.py
+++ EXPENSE TRACKER.py
@@ -1,15 +1,17 @@
+#creating list named "expenses" to store and track
 expenses = []
 
-
+#creating a while loop
 while True:
     print("\n Expense Tracker Menu")
     print("\n 1.Add Expense \n 2.view Expenses \n 3.Total Expense \n 4.Exit\n")
-
+#variable for storing the users input to the menu
     choice = input("Enter the choice( 1,2,3,4):")
-
+#conditional statements
     if choice == "1":
         item = input("Enter Expense Name:")
         amount = float(input("Enter Expense Amount:"))
+        #appending the item and item ammount
         expenses.append([item,amount])
         print("Expense is added to your list..")
     elif choice== "2":
@@ -17,10 +19,12 @@ while True:
             print("No expenses yet.")
         else:
             print("\nYour Expenses:")
+            #printing the expenses which are stored previously
             for expense in expenses:
                 print(f"{expense[0]} - ₹{expense[1]}")
 
     elif choice == "3":
+        #adding all the expenses in the list 
         total = sum([expense[1] for expense in expenses])
         print(f"Total Expenses: ₹{total}")
 
@@ -30,4 +34,5 @@ while True:
 
     else:
         print("Invalid choice!!")
+
 
